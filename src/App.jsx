df23e3da9de649054d2cbf763fd771dde9ef93ca
Purice Victor <pvictor0110@gmail.com>
2023-09-17 14:43:35 +0300

    cleaned up Altcoinx project

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1 className='w-full h-screen text-6xl text-black-700 flex items-center justify-center'>Altcoinx</h1>
+      <h2 className="w-full h-screen text-4xl text-black-950 flex items-center justify-center">Project</h2>
+    </div>
   )
 }
 
