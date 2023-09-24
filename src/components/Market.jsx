7eb982c4daabbde53bac885c773ec06d87c4f55d
Purice Victor <pvictor0110@gmail.com>
2023-09-24 22:33:30 +0300

    Update Market.jsx

--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -7,7 +7,7 @@ const [data, setData] = useState(null);
 const [search, setSearch] = useState('')
 
 
-const urlTrend = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&locale=en';
+const urlTrend = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en';
 
 useEffect(() => {
     axios.get(urlTrend).then((response) => {
