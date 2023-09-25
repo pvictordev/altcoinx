aee318e3ddb3a4817edef75e7858611519a1eb14
Purice Victor <pvictor0110@gmail.com>
2023-09-25 21:10:53 +0300

    Update Crypto.jsx

--- src/pages/Crypto.jsx
+++ src/pages/Crypto.jsx
@@ -21,85 +21,72 @@ const Crypto = () => {
     })
   },[])
   // console.log(coin)
-
+  
 
   return (
+    
     <section className='crypto w-full h-screen bg-gradient-to-b from-custom-end to-custom-start overflow-hidden'>
 
         <button className='absolute top-3 left-3' onClick={() => history(-1)}><AiOutlineArrowLeft style={{fontSize:'1.5rem'}}/></button>
 
-        <div className="crypto__content  w-full h-screen flex justify-center items-center p-20 gap-20">
-          <div className="content__crypto flex flex-col items-center text-left">
+        <div className="crypto__content  w-full h-screen flex justify-between items-center p-20 pt-24 gap-20">
+          <div className="content__crypto flex flex-col items-center text-left gap-3">
 
-            {coin.image ? <img src={coin.image.large} className='w-20 h-20' alt=""/> : null }
-            <h2 className='text-2xl'>{coin.name}</h2>
-            <h3 className='text-xl'>Rank : {coin.market_cap_rank}</h3>
+            {coin.image ? <img src={coin.image.large} className='w-24 h-24' alt=""/> : null }
+            <h2 className='text-3xl'>{coin.name}</h2>
 
-            <ul className="chart__statistics text-left gap-2">
+
+            <ul className="chart__statistics text-left mt-4">
+
+
+              <li className='text-md mb-1'>Symbol : {coin.symbol}</li>
+              <li className='text-md mb-1'>Rank : #{coin.market_cap_rank}</li>
               
-              <li className='text-lg'>
-                Current Price: {
+              <li className='text-md mb-1'>
+                Current Price : ${
                 coin.market_data?.current_price ? coin.market_data.current_price.usd : null
                 } 
               </li>
 
-              <li className='text-lg'>1h %: 
-                {coin.market_data?.price_change_percentage_1h_in_currency ? coin.market_data.price_change_percentage_1h_in_currency.usd : null
-                }
+              <li className='text-md mb-1'>1h Change : {coin.market_data?.price_change_percentage_1h_in_currency ? coin.market_data.price_change_percentage_1h_in_currency.usd : null
+              }%
               </li>
-
-              <li className='text-lg'>24h %: 
-                {coin.market_data?.price_change_percentage_24h_in_currency ? coin.market_data.price_change_percentage_24h_in_currency.usd : null
-                }
+              <li className='text-md mb-1'>24h Change : {coin.market_data?.price_change_percentage_24h_in_currency ? coin.market_data.price_change_percentage_24h_in_currency.usd : null
+              }%
               </li>
-
-              <li className='text-lg'>7d %: 
-                {coin.market_data?.price_change_percentage_7d_in_currency ? coin.market_data.price_change_percentage_7d_in_currency.usd : null
-                }
+              <li className='text-md mb-1'>7d Change : {coin.market_data?.price_change_percentage_7d_in_currency ? coin.market_data.price_change_percentage_7d_in_currency.usd : null
+              }%
               </li>
-
-              <li className='text-lg'>14d %: 
-                {coin.market_data?.price_change_percentage_14d_in_currency ? coin.market_data.price_change_percentage_14d_in_currency.usd : null
-                }
+              <li className='text-md mb-1'>14d Change : {coin.market_data?.price_change_percentage_14d_in_currency ? coin.market_data.price_change_percentage_14d_in_currency.usd : null
+                }%
               </li>
-
-              <li className='text-lg'>30d %: 
-                {coin.market_data?.price_change_percentage_30d_in_currency ? coin.market_data.price_change_percentage_30d_in_currency.usd : null
-                }
+              <li className='text-md mb-1'>30d Change : {coin.market_data?.price_change_percentage_30d_in_currency ? coin.market_data.price_change_percentage_30d_in_currency.usd : null
+              }% 
               </li>
-
-              <li className='text-lg'>1y %: 
-                {coin.market_data?.price_change_percentage_1y_in_currency ? coin.market_data.price_change_percentage_1y_in_currency.usd : null
-                }
+              <li className='text-md mb-1'>1y Change : {coin.market_data?.price_change_percentage_1y_in_currency ? coin.market_data.price_change_percentage_1y_in_currency.usd : null
+              }%
               </li>
           
-                
-              <li className='text-lg'>
-                24h low:{coin.market_data?.low_24h ? <p>$ {coin.market_data.low_24h.usd}</p>: null}
-              </li>              
+              <li className='text-md mb-1'>
+                24h high : ${coin.market_data?.high_24h ? coin.market_data.high_24h.usd: null}
+              </li>  
+              <li className='text-md mb-1'>
+                24h low : ${coin.market_data?.low_24h ? coin.market_data.low_24h.usd: null}
+              </li> 
+                          
 
-              <li className='text-lg'>Market Cap: 
-                {coin.market_data?.market_cap ? <p>$ {coin.market_data.market_cap.usd}</p> : null}
+              <li className='text-md mb-1'>Market Cap: ${coin.market_data?.market_cap ? coin.market_data.market_cap.usd: null}
               </li>
-              
-              <li className='text-lg'>Circulating Supply: 
-                {coin.market_data ? <p>{coin.market_data.circulating_supply}</p>: null}
+  
+              <li className='text-md mb-1'>Circulating Supply : {coin.market_data ? coin.market_data.circulating_supply : null} 
               </li>
-
-              <li className='text-lg'>Total Supply: 
-                {coin.market_data ? <p>{coin.market_data.total_supply}</p>: null}
-              </li>
-
-              {/* <li>Total Volume: 
-                {coin.market_data ? <p>{coin.market_data.total_volume}</p>: null}
-              </li> */}
 
             </ul>
 
           </div>
 
           <div className='content__chart'>
-              <div className=''>Chart</div>
+              <div className=''>chart</div>
           </div>
         </div>
     </section>
