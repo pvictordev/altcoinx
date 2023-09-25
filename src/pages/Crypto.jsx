508016627833597d40a2ea8624075a10609fc092
Purice Victor <pvictor0110@gmail.com>
2023-09-25 20:34:36 +0300

    added chart

--- src/pages/Crypto.jsx
+++ src/pages/Crypto.jsx
@@ -30,32 +30,76 @@ const Crypto = () => {
 
         <div className="crypto__content  w-full h-screen flex justify-center items-center p-20 gap-20">
           <div className="content__crypto flex flex-col items-center text-left">
-            {coin.image ? <img src={coin.image.large} alt=""/> : null }
-            <h2>{coin.name}</h2>
-            <ul className="chart__statistics">
-              <li>rank: {coin.market_cap_rank}</li>
-              <li>Current Price: {coin.market_data.current_price} 
+
+            {coin.image ? <img src={coin.image.large} className='w-20 h-20' alt=""/> : null }
+            <h2 className='text-2xl'>{coin.name}</h2>
+            <h3 className='text-xl'>Rank : {coin.market_cap_rank}</h3>
+
+            <ul className="chart__statistics text-left gap-2">
+              
+              <li className='text-lg'>
+                Current Price: {
+                coin.market_data?.current_price ? coin.market_data.current_price.usd : null
+                } 
               </li>
 
+              <li className='text-lg'>1h %: 
+                {coin.market_data?.price_change_percentage_1h_in_currency ? coin.market_data.price_change_percentage_1h_in_currency.usd : null
+                }
+              </li>
 
-              <li>1h %: {coin.market_data.price_change_percentage_1h_in_currency.usd}</li>
-              <li>24h %: {coin.market_data.price_change_percentage_24h_in_currency.usd}</li>
-              <li>7d %: {coin.market_data.price_change_percentage_7d_in_currency.usd}</li>
-              <li>14d %: {coin.market_data.price_change_percentage_14d_in_currency.usd}</li>
-              <li>30d %: {coin.market_data.price_change_percentage_30d_in_currency.usd}</li>
-              <li>1y %: {coin.market_data.price_change_percentage_1y_in_currency.usd}</li>
+              <li className='text-lg'>24h %: 
+                {coin.market_data?.price_change_percentage_24h_in_currency ? coin.market_data.price_change_percentage_24h_in_currency.usd : null
+                }
+              </li>
 
-              <li>24h high: {coin.market_data.high_24.usd}</li>               
-              <li>24h low: {coin.market_data.low_24.usd}</li>              
+              <li className='text-lg'>7d %: 
+                {coin.market_data?.price_change_percentage_7d_in_currency ? coin.market_data.price_change_percentage_7d_in_currency.usd : null
+                }
+              </li>
 
-              <li>Market Cap: {coin.market_data.market_cap.usd}</li>
-              <li>Circulating Supply: {coin.market_data.circulating_supply}</li>
-              <li>Total Supply: {coin.market_data.total_supply}</li>
-              <li>Total Volume: {coin.market_data.total_volume}</li>              
+              <li className='text-lg'>14d %: 
+                {coin.market_data?.price_change_percentage_14d_in_currency ? coin.market_data.price_change_percentage_14d_in_currency.usd : null
+                }
+              </li>
+
+              <li className='text-lg'>30d %: 
+                {coin.market_data?.price_change_percentage_30d_in_currency ? coin.market_data.price_change_percentage_30d_in_currency.usd : null
+                }
+              </li>
+
+              <li className='text-lg'>1y %: 
+                {coin.market_data?.price_change_percentage_1y_in_currency ? coin.market_data.price_change_percentage_1y_in_currency.usd : null
+                }
+              </li>
+          
+                
+              <li className='text-lg'>
+                24h low:{coin.market_data?.low_24h ? <p>$ {coin.market_data.low_24h.usd}</p>: null}
+              </li>              
+
+              <li className='text-lg'>Market Cap: 
+                {coin.market_data?.market_cap ? <p>$ {coin.market_data.market_cap.usd}</p> : null}
+              </li>
+              
+              <li className='text-lg'>Circulating Supply: 
+                {coin.market_data ? <p>{coin.market_data.circulating_supply}</p>: null}
+              </li>
+
+              <li className='text-lg'>Total Supply: 
+                {coin.market_data ? <p>{coin.market_data.total_supply}</p>: null}
+              </li>
+
+              {/* <li>Total Volume: 
+                {coin.market_data ? <p>{coin.market_data.total_volume}</p>: null}
+              </li> */}
+
             </ul>
+
           </div>
+
           <div className='content__chart'>
-              <div className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum odio vel distinctio aspernatur enim id tenetur ipsam repudiandae dignissimos nulla voluptatibus veritatis natus in omnis illo, labore ratione sunt cum!</div>
+              <div className=''>Chart</div>
           </div>
         </div>
     </section>
