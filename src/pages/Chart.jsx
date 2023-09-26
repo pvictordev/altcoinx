0123c22e1bd1ae22a1001947743945052c6b8a97
Purice Victor <pvictor0110@gmail.com>
2023-09-26 23:23:53 +0300

    Update Chart.jsx

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -32,38 +32,60 @@ const Chart = () => {
 
     const chartParams = useParams();
 
-    const historyURL = `https://api.coingecko.com/api/v3/coins/${chartParams.coinId}/market_chart?vs_currency=usd&days=1`; 
+    // const historyURL = `https://api.coingecko.com/api/v3/coins/${chartParams.coinId}/market_chart?vs_currency=usd&days=1`; 
 
-    useEffect(() => {
-        axios.get(historyURL).then((res) => {
-            const data = res.data
-            const chartData = data.prices.map(value=>({
-                x: value[0],
-                y: value[1].toFixed(2)
+    // useEffect(() => {
+    //     axios.get(historyURL).then((res) => {
+    //         const data = res.data
+    //         const chartData = data.prices.map(value=>({
+    //             x: value[0],
+    //             y: value[1].toFixed(2)
+    //         }));
+    //         setChart(chartData)
+    //     }).catch((error) => {
+    //       console.log('error')
+    //     })
+    // },[])
+    const fetchData = (days) => {
+        const historyURL = `https://api.coingecko.com/api/v3/coins/${chartParams.coinId}/market_chart?vs_currency=usd&days=${days}`;
+        axios
+          .get(historyURL)
+          .then((res) => {
+            const data = res.data;
+            const chartData = data.prices.map((value) => ({
+              x: moment(value[0]).format('MMM DD, HH:mm'),
+              y: value[1].toFixed(2),
             }));
-            setChart(chartData)
-        }).catch((error) => {
-          console.log('error')
-        })
-    },[])
-
+            setChart(chartData);
+          })
+          .catch((error) => {
+            console.log('error', error);
+          });
+      };
     
+      useEffect(() => {
+    
+        fetchData(1);
+      }, []);
 
 
     const options = {
     responsive: true,
+    elements: {
+        point: {
+          radius: 1,
+        },
+      },
     plugins: {
         legend: {
         position: 'top',
         },
         title: {
         display: true,
-        text: 'Chart.js Line Chart',
         },
     },
     };
     
-    // chart.map(value=>moment(value.x).format('MMMDD')),
     const data = {
      
         labels: chart.map(value=>moment(value.x).format('MMMDD')),
@@ -86,10 +108,17 @@ const Chart = () => {
     <div className='chart flex justify-center flex-col'>
         <Line options={options} data={data}/>
         <div className='chart__buttons flex justify-between mt-10'>
-            <button className='border-2 p-2 rounded-xl hover:bg-indigo-600 hover:bg-opacity-50'>24 hours</button>
-            <button className='border-2 p-2 rounded-xl hover:bg-indigo-600 hover:bg-opacity-50'>30 days</button>
-            <button className='border-2 p-2 rounded-xl hover:bg-indigo-600 hover:bg-opacity-50'>3 months</button>
-            <button className='border-2 p-2 rounded-xl hover:bg-indigo-600 hover:bg-opacity-50'>1 year</button>
+            <button className='border-2 p-2 rounded-xl hover:bg-indigo-600 hover:bg-opacity-50'
+            onClick={()=> fetchData(1)}>24 hours</button>
+
+            <button className='border-2 p-2 rounded-xl hover:bg-indigo-600 hover:bg-opacity-50'
+            onClick={()=> fetchData(30)}>30 days</button>
+
+            <button className='border-2 p-2 rounded-xl hover:bg-indigo-600 hover:bg-opacity-50'
+            onClick={()=> fetchData(90)}>3 months</button>
+
+            <button className='border-2 p-2 rounded-xl hover:bg-indigo-600 hover:bg-opacity-50'
+            onClick={()=> fetchData(365)}>1 year</button>
 
         </div>
     </div>
