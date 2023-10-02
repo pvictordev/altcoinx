bd5cacef9f4fe87419438bcddfa2ba54801c3bb1
Purice Victor <pvictor0110@gmail.com>
2023-10-02 18:54:56 +0300

    Update Market.jsx

--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -37,9 +37,9 @@ const dataElems = filteredData.splice(0,10).map(elems => {
       to={`/coin/${elems.id}`} 
       element={<Crypto/>}
       >
-        <li className='flex flex-row justify-between py-8 px-4 text-center border-b border-double hover:bg-indigo-600 hover:bg-opacity-10 items-center xxs-max:justify-center' key={elems.id} >
+        <li className='flex flex-row justify-between py-8 px-4 text-center border-b border-double hover:bg-indigo-600 hover:bg-opacity-10 items-center xs-max:justify-center' key={elems.id} >
             <p className='text-2xl flex items-center gap-2 sm-max:text-xl'><img src={elems.image} alt="" className="w-12 inline sm-max:w-8" /> {elems.name}</p>
-            <p className='text-2xl items-center flex sm-max:text-xl xxs-max:hidden'>$ {elems.current_price.toLocaleString('en-US')}</p>
+            <p className='text-2xl items-center flex sm-max:text-xl xs-max:hidden'>$ {elems.current_price.toLocaleString('en-US')}</p>
             <p className={`text-2xl sm-max:hidden ${elems.price_change_percentage_24h < 0? 'text-red-600' : 'text-green-500'}`}>{elems.price_change_percentage_24h}%</p>
             <p className='text-2xl l-max:hidden'>$ {elems.market_cap.toLocaleString('en-US')}</p>
         </li>
@@ -49,13 +49,13 @@ const dataElems = filteredData.splice(0,10).map(elems => {
 
   return (
     <section id='market' className="market bg-gradient-to-b from-custom-start to-custom-start">
-        <div className="market__container flex flex-col py-20 px-20 xxs-max:px-10">
+        <div className="market__container flex flex-col py-20 px-20 xs-max:px-10">
             <div className="market__content">
                 <h2 className='text-4xl font-bold mb-10'>Market</h2>
                 
                 <input 
                   type="text" 
-                  className='mb-10 w-full bg-indigo-600 bg-opacity-10 p-3 text-2xl xxs-max:text-sm border-solid border-2 border-white rounded-xl focus:outline-none focus:border-purple-600' 
+                  className='mb-10 w-full bg-indigo-600 bg-opacity-10 p-3 text-xl xs-max:text-sm border-solid border-2 border-white rounded-xl focus:outline-none focus:border-purple-600' 
                   value={search}
                   onChange={handleSearch}
                   placeholder='Search Cryptocurrency'
@@ -63,9 +63,9 @@ const dataElems = filteredData.splice(0,10).map(elems => {
 
                 <div className="market__list flex flex-col">
 
-                    <div className="list__top flex flex-row justify-between bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600 p-4 rounded-t-lg whitespace-break-spaces xxs-max:justify-center">
+                    <div className="list__top flex flex-row justify-between bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600 p-4 rounded-t-lg whitespace-break-spaces xs-max:justify-center">
                         <p className='text-2xl'>Coin</p>
-                        <p className='text-2xl xxs-max:hidden'>Price</p>
+                        <p className='text-2xl xs-max:hidden'>Price</p>
                         <p className='text-2xl sm-max:hidden'>24h %</p>
                         <p className='text-2xl l-max:hidden'>Market Cap</p>
                     </div>
